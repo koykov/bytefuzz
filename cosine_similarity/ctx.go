25eb10dd7192a98ad356721d1349b552dd635eca
Konstantin Voykov <koykov0@gmail.com>
2024-12-08 23:47:28 +0300

    cosine_similarity: ctx/pool draft

---
+++ cosine_similarity/ctx.go
@@ -0,0 +1,11 @@
+package cosine_similarity
+
+type Ctx struct {
+	// todo implement me
+}
+
+func NewCtx() *Ctx {
+	return &Ctx{}
+}
+
+func (ctx *Ctx) Reset() {}
---
+++ cosine_similarity/pool.go
@@ -0,0 +1,21 @@
+package cosine_similarity
+
+import "sync"
+
+type pool struct {
+	p sync.Pool
+}
+
+var p = pool{p: sync.Pool{New: func() interface{} { return &Ctx{} }}}
+
+func Acquire() *Ctx {
+	return p.p.Get().(*Ctx)
+}
+
+func Release(x *Ctx) {
+	if x == nil {
+		return
+	}
+	x.Reset()
+	p.p.Put(x)
+}
