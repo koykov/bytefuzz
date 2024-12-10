99d06e972c97bd804d1fec1ca698be3487c9dc01
Konstantin Voykov <koykov0@gmail.com>
2024-12-10 23:37:47 +0300

    jaro_winkler: implementation ...

--- go.mod
+++ go.mod
@@ -3,6 +3,12 @@ module github.com/koykov/bytefuzz
 go 1.18
 
 require (
+	github.com/koykov/bytealg v1.0.7-0.20241210200050-a6d4ea34309d
 	github.com/koykov/byteconv v1.0.1
 	github.com/koykov/openrt v0.0.0-20240728195600-ccc3242946f9
+)
+
+require (
+	github.com/koykov/byteseq v1.0.1 // indirect
+	github.com/koykov/entry v1.0.2 // indirect
 )
--- jaro_winkler/ctx.go
+++ jaro_winkler/ctx.go
@@ -1,11 +1,67 @@
 package jaro_winkler
 
+import (
+	"math"
+	"unicode/utf8"
+
+	"github.com/koykov/bytealg"
+	"github.com/koykov/byteconv"
+)
+
 type Ctx struct {
-	// todo implement me
+	buf  []byte
+	bufr []rune
+	bufb []bool
 }
 
 func NewCtx() *Ctx {
 	return &Ctx{}
 }
 
-func (ctx *Ctx) Reset() {}
+func (ctx *Ctx) Distance(text, target []byte) int {
+	text = bytealg.ToLowerBytes(text)
+	target = bytealg.ToLowerBytes(target)
+	return ctx.dist(text, target)
+}
+
+func (ctx *Ctx) DistanceString(text, target string) int {
+	ctx.buf = append(ctx.buf, text...)
+	ctx.buf = append(ctx.buf, target...)
+	return ctx.Distance(ctx.buf[:len(text)], ctx.buf[len(text):])
+}
+
+func (ctx *Ctx) dist(p1, p2 []byte) int {
+	rl1, rl2 := utf8.RuneCount(p1), utf8.RuneCount(p2)
+	if rl1 > rl2 {
+		p1, p2 = p2, p1
+		rl1, rl2 = rl2, rl1
+	}
+
+	ctx.bufr = byteconv.AppendBytesToRunes(ctx.bufr, p1)
+	ctx.bufr = byteconv.AppendBytesToRunes(ctx.bufr, p2)
+	r1, r2 := ctx.bufr[:rl1], ctx.bufr[rl1:]
+
+	var mc int
+	for i := 0; i < rl1; i++ {
+		var m bool
+		if r1[i] == r2[i] {
+			mc, m = mc+1, true
+		}
+		ctx.bufb = append(ctx.bufb, m)
+	}
+	if mc == 0 {
+		return 0
+	}
+
+	w := int(math.Floor(math.Max(float64(rl1), float64(rl2))/2) - 1)
+	_ = w
+	// todo ...
+
+	return 0
+}
+
+func (ctx *Ctx) Reset() {
+	ctx.buf = ctx.buf[:0]
+	ctx.bufr = ctx.bufr[:0]
+	ctx.bufb = ctx.bufb[:0]
+}
