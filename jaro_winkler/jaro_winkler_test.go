dfd346cc4984cdfcfb7842fc42074b6426857998
Konstantin Voykov <koykov0@gmail.com>
2024-12-13 23:43:27 +0300

    jaro_winkler: testing draft

---
+++ jaro_winkler/jaro_winkler_test.go
@@ -0,0 +1,23 @@
+package jaro_winkler
+
+import "testing"
+
+var stages = []struct {
+	text, target string
+	distance     float64
+}{
+	{"apple", "applet", 0.9722222222222223},
+}
+
+func TestJaroWinkler(t *testing.T) {
+	for _, st := range stages {
+		t.Run(st.text, func(t *testing.T) {
+			ctx := Acquire()
+			defer Release(ctx)
+			dist := ctx.DistanceString(st.text, st.target)
+			if dist != st.distance {
+				t.Errorf("distance: got %f, want %f", dist, st.distance)
+			}
+		})
+	}
+}
