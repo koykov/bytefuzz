26098641688ce0fe2ec289e4f8233377f7480db0
Konstantin Voykov <koykov0@gmail.com>
2024-12-16 00:18:01 +0300

    jaro_winkler: tests issue

--- jaro_winkler/jaro_winkler_test.go
+++ jaro_winkler/jaro_winkler_test.go
@@ -7,6 +7,13 @@ var stages = []struct {
 	distance     float64
 }{
 	{"apple", "applet", 0.9722222222222223},
+	{"Michael", "Michelle", 0.8880952380952382},
+	{"McDonald's", "Mcdonells", 0.8777777777777778},
+	{"hello", "world", 0.4666666666666666},
+	{"telephone", "telephne", 0.9346560846560846},
+	{"AI", "Artificial Intelligence", 0},
+	{"abacus", "abaxus", 0.8444444444444443},
+	{"Thompson", "Thomson", 0.8278571428571428},
 }
 
 func TestJaroWinkler(t *testing.T) {
@@ -15,6 +22,7 @@ func TestJaroWinkler(t *testing.T) {
 			ctx := NewCtx()
 			dist := ctx.DistanceString(st.text, st.target)
 			if dist != st.distance {
+				t.Log(dist)
 				t.Errorf("distance: got %f, want %f", dist, st.distance)
 			}
 		})
